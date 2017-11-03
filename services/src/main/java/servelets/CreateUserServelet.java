80474f72acf4e5a3cdc73c44cf532a39c7b1dbb9
IssamArch <bi106809@etu.unice.fr>
2017-11-03 15:03:48 +0100

    debug user create servlet

--- services/src/main/java/servelets/CreateUserServelet.java
+++ services/src/main/java/servelets/CreateUserServelet.java
@@ -5,7 +5,7 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.users.User;
+
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 
@@ -18,19 +18,18 @@ import javax.servlet.http.HttpServletResponse;
 import com.googlecode.objectify.ObjectifyService;
 import model.User;
 
+
 public class CreateUserServelet extends HttpServlet {
   // Process the http POST of the form
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     User utilisateur;
-    UserService userService = UserServiceFactory.getUserService();
-    User user = userService.getCurrentUser();  
 
     String username = req.getParameter("username");
     String mail = req.getParameter("mail");  
-    String offer = req.getParameter("offer");
+    int offer = Integer.parseInt(req.getParameter("offer"));
 
-    utilisateur = new User(username, mail, offer);
+    utilisateur = new User(username,mail,offer);
   
     ObjectifyService.ofy().save().entity(utilisateur).now();
   }
