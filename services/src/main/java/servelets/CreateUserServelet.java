a86cf6897dce1394ad82ffd2b18e4871e2d156b1
Elmrihy <elmrihy.mohamed.01@gmail.com>
2017-11-03 14:56:05 +0100

    UserServlet

--- services/src/main/java/servelets/CreateUserServelet.java
+++ services/src/main/java/servelets/CreateUserServelet.java
@@ -17,15 +17,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.googlecode.objectify.ObjectifyService;
 import model.User;
-/**
- * Form Handling Servlet
- * Most of the action for this sample is in webapp/guestbook.jsp, which displays the
- * {@link Greeting}'s. This servlet has one method
- * {@link #doPost(<#HttpServletRequest req#>, <#HttpServletResponse resp#>)} which takes the form
- * data and saves it.
- */
-public class SignGuestbookServlet extends HttpServlet {
 
+public class CreateUserServelet extends HttpServlet {
   // Process the http POST of the form
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
